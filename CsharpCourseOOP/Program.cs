c662275d21a4eb8d0690eb48171e508af7aa1461
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-01-31 17:15:08 +0300

    homework methods and functions

--- CsharpCourseOOP/Program.cs
+++ CsharpCourseOOP/Program.cs
@@ -2,44 +2,67 @@
 
 namespace CsharpCourseOOP
 {
-    internal class Program
+    public class Program
     {
         public void Main(string[] args)
         {
-            Notebook asus = new Notebook();
+            var asus = new Notebook();
             asus.processor = "Intel core i7 11800h";
             asus.ozu = 32;
             asus.videoCart = "Nvidia RTX3050Ti";
             asus.screen = 16;
             asus.battery = "90Wh";
+            asus.price = 150000;
 
-            Phone xiaomi = new Phone();
+            var xiaomi = new Phone();
             xiaomi.screen = 6;
             xiaomi.battery = 5000;
             xiaomi.memory = 128;
             xiaomi.ozu = 4;
+            xiaomi.price = 20000;
 
-            Tv samsung = new Tv();
+            var samsung = new Tv();
             samsung.screen = 55;
             samsung.screenRaz = "1280x720";
+            samsung.price = 30000;
 
-            Magazine f5it = new Magazine();
+            var f5it = new Magazine();
             f5it.personal = 150;
             f5it.moneyInYear = 110000000;
             f5it.webSite = "www.f5it.ru";
             f5it.cityOfOffice = "Novosibirsk";
             f5it.cityOfOffice = "Moscow";
 
-            Garnitura lg = new Garnitura();
+            var lg = new Garnitura();
             lg.withOutprovod = true;
             lg.timeOfWorking = "1 week";
             lg.color = "black";
             lg.typeOfEarsIsIneer = false;
+            lg.price = 2500;
+
+            int stockPrice(int nbCount, int phoneCount, int tvCount, int earsCount)
+            {
+                int totalPrice = 0;
+                int nbPrice = asus.price * nbCount;
+                int phonePrice = xiaomi.price * phoneCount;
+                int tvPrice = samsung.price * tvCount;
+                int earsPrice = lg.price * earsCount;
+                totalPrice = nbPrice + phonePrice + tvPrice + earsPrice;
+                return totalPrice;
+            }
+
+            int januaryDate30012023 = stockPrice(17, 50, 25, 30);
+
+            Console.WriteLine(januaryDate30012023);
+            Console.ReadLine();
         }
+
+
     }
 
     internal class Garnitura
     {
+        public int price { set; get; }
         public bool typeOfEarsIsIneer { get; set; }
         public string color { get; set; }
         public string timeOfWorking { get; set; }
@@ -56,12 +79,14 @@ namespace CsharpCourseOOP
 
     internal class Tv
     {
+        public int price { get; set; }
         public string screenRaz { get; set; }
         public int screen { get; set; }
     }
 
     internal class Phone
     {
+        public int price { set; get; }
         public int memory { get; set; }
         public int ozu { get; set; }
         public int screen { get; set; }
@@ -70,6 +95,7 @@ namespace CsharpCourseOOP
 
     internal class Notebook
     {
+        public int price { get; set; }
         public int screen { get; set; }
         public string battery { get; set; }
         public string videoCart { get; set; }
@@ -77,3 +103,4 @@ namespace CsharpCourseOOP
         public string processor { get; set; }
     }
 }
+
